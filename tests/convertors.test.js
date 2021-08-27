cbfc61aa4cdf9f4a78d1b5c947a146ece3d144a3
Owen Jones <owencjones@gmail.com>
2021-08-27 16:04:27 +0100

    Convertors outlines in progress

---
+++ tests/convertors.test.js
@@ -0,0 +1,14 @@
+import {
+    currencyToUSD,
+    usdToCurrency
+} from '../lib/convertors';
+
+describe('Convertor tests', () => {
+    describe('usdToCurrency', () => {
+        it.todo('tests');
+    });
+
+    describe('currencyToUSD', () => {
+        it.todo('tests');
+    });
+});
