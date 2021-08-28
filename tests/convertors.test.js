f414ee30493fe385858823974dee799dac2f38de
Owen Jones <owencjones@gmail.com>
2021-08-28 12:44:20 +0100

    Extra tests for util functions

--- tests/convertors.test.js
+++ tests/convertors.test.js
@@ -1,8 +1,13 @@
 const {
     currencyToUSD,
     usdToCurrency,
-    currencyToCurrency
+    currencyToCurrency,
+
+    requireValidNumber,
+    requireValidCurrencyCode,
+    requireValidCurrencyObject
 } = require('../lib/convertors');
+const { InputException } = require('../lib/exceptions');
 
 describe('Convertor tests', () => {
 
@@ -21,15 +26,15 @@ describe('Convertor tests', () => {
 
         describe('Sad path scenarios', () => {
             it('should throw an error if currency code is not in data set', () => {
-                expect(() => usdToCurrency(15, 'NONEXISTENTCURRENCYCODE', exampleConversionData)).toThrow();
+                expect(() => usdToCurrency(15, 'NONEXISTENTCURRENCYCODE', exampleConversionData)).toThrow(InputException);
             });
 
             it('should throw an error if amount is not a number', () => {
-                expect(() => usdToCurrency('NaN', 'ABC', exampleConversionData)).toThrow();
+                expect(() => usdToCurrency('NaN', 'ABC', exampleConversionData)).toThrow(InputException);
             });
 
             it('should throw an error if dataset is not an object', () => {
-                expect(() => usdToCurrency(15, 'ABC', 'not an object')).toThrow();
+                expect(() => usdToCurrency(15, 'ABC', 'not an object')).toThrow(InputException);
             });
         });
     });
@@ -43,15 +48,15 @@ describe('Convertor tests', () => {
 
         describe('Sad path scenarios', () => {
             it('should throw an error if currency code is not in data set', () => {
-                expect(() => currencyToUSD(15, 'NONEXISTENTCURRENCYCODE', exampleConversionData)).toThrow();
+                expect(() => currencyToUSD(15, 'NONEXISTENTCURRENCYCODE', exampleConversionData)).toThrow(InputException);
             });
 
             it('should throw an error if amount is not a number', () => {
-                expect(() => currencyToUSD('NaN', 'ABC', exampleConversionData)).toThrow();
+                expect(() => currencyToUSD('NaN', 'ABC', exampleConversionData)).toThrow(InputException);
             });
 
             it('should throw an error if dataset is not an object', () => {
-                expect(() => currencyToUSD(15, 'ABC', 'not an object')).toThrow();
+                expect(() => currencyToUSD(15, 'ABC', 'not an object')).toThrow(InputException);
             });
         });
     });
@@ -69,20 +74,50 @@ describe('Convertor tests', () => {
 
         describe('Sad path scenarios', () => {
             it('should throw an error if first currency code is not in data set', () => {
-                expect(() => currencyToCurrency(15, 'NONEXISTENTCURRENCYCODE', 'ABC', exampleConversionData)).toThrow();
+                expect(() => currencyToCurrency(15, 'NONEXISTENTCURRENCYCODE', 'ABC', exampleConversionData)).toThrow(InputException);
             });
 
             it('should throw an error if second currency code is not in data set', () => {
-                expect(() => currencyToCurrency(15, 'ABC', 'NONEXISTENTCURRENCYCODE', exampleConversionData)).toThrow();
+                expect(() => currencyToCurrency(15, 'ABC', 'NONEXISTENTCURRENCYCODE', exampleConversionData)).toThrow(InputException);
             });
 
             it('should throw an error if amount is not a number', () => {
-                expect(() => currencyToCurrency('NaN', 'ABC', 'DEF', exampleConversionData)).toThrow();
+                expect(() => currencyToCurrency('NaN', 'ABC', 'DEF', exampleConversionData)).toThrow(InputException);
             });
 
             it('should throw an error if dataset is not an object', () => {
-                expect(() => currencyToCurrency(15, 'ABC', 'DEF', 'not an object')).toThrow();
+                expect(() => currencyToCurrency(15, 'ABC', 'DEF', 'not an object')).toThrow(InputException);
             });
         });
     });
+
+    describe('Utility functions', () => {
+        describe('requireValidNumber', () => {
+            it('should return nothing if passed a number', () => expect(requireValidNumber(15)).not.toThrow());
+
+            it('should throw an input error if passed an invalid number', () => expect(requireValidNumber('Not a valid number')).toThrow(InputException));
+        });
+
+        describe('requireValidCurrencyCode', () => {
+            it ('should return nothing if passed a currency code', () => expect(requireValidCurrencyCode('ABC')).not.toThrow());
+
+            it('should throw an input error if passed a non string', () => expect(requireValidCurrencyCode(1)).toThrow(InputException));
+
+            it('should throw an input error if passed a too-short string', () => expect(requireValidCurrencyCode('AB')).toThrow(InputException));
+
+            it('should throw an input error if passed a too-long string', () => expect(requireValidCurrencyCode('ABCD')).toThrow(InputException));
+
+            it('should throw an input error if passed a string containing invalid characters', () => expect(requireValidCurrencyCode('AB1')).toThrow(InputException));
+        });
+
+        describe('requireValidCurrencyObject', () => {
+            it('should return if the object is valid', () => expect(requireValidCurrencyObject({ABC: 1})))
+
+            it('should require an Object', () => expect(requireValidCurrencyObject('ABC')).toThrow(InputException));
+
+            it('should require an Object in which the keys are valid currencies', () => expect(requireValidCurrencyObject({ AB1: 1 })).toThrow(InputException));
+
+            it('should require an Object in which the value are valid numbers', () => expect(requireValidCurrencyObject({ABC: 'ab'})).toThrow(InputException));
+        })
+    })
 });
